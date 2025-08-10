10981e8133f0c181ca69adb0183029ce19478665
Irench1k <irena.yarlyk@gmail.com>
2025-08-10 15:40:21 +0200

    Basic Django demonstration: implement various Django route examples

---
+++ languages/python/django/basic/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # Example route
+    path('hello/', views.hello, name='hello'),
+    path('user/<str:username>/', views.show_user, name='show_user'),
+    path('user/<str:username>/<int:user_id>/', views.show_user_details, name='show_user_details'),
+    path('user/', views.show_user_with_params, name='show_user_with_params'),
+    path('submit/', views.submit, name='submit'),
+]
---
+++ languages/python/django/basic/myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+# `/`
+def index(request):
+    return HttpResponse(f"Index Page, Route: {request.path}")
+
+# `/hello`
+def hello(request):
+    return HttpResponse(f"Hello, world!, Route: {request.path}")
+
+# `/user/<username>`
+def show_user(request, username):
+    return HttpResponse(f"User: {username}, Route: {request.path}")
+
+# `/user/<username>/<int:user_id>`
+def show_user_details(request, username, user_id):
+    return HttpResponse(f"Username: {username}, User ID: {user_id}, Route: {request.path}")
+
+# `/user?id=123&type=admin`
+def show_user_with_params(request):
+    user_id = request.GET.get('id')
+    user_type = request.GET.get('type')
+    return HttpResponse(f"User ID: {user_id}, User Type: {user_type}, Route: {request.path}")
+    
+@csrf_exempt  # exempt the view from CSRF protection for simplicity
+def submit(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', 'No Name')
+        return redirect(reverse('show_user', kwargs={'username': name}))
+    else:
+        return render(request, 'myapp/submit.html')
