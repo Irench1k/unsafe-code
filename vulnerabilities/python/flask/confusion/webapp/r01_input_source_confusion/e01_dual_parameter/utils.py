dad4cbebe6d0b9d962cba412f85af90ecb65e07d
Irina Iarlykanova <irina.iarlykanova@gmail.com>
2025-11-09 17:56:00 +0100

    Improved readability of helper functions

--- vulnerabilities/python/flask/confusion/webapp/r01_input_source_confusion/e01_dual_parameter/utils.py
+++ vulnerabilities/python/flask/confusion/webapp/r01_input_source_confusion/e01_dual_parameter/utils.py
@@ -4,60 +4,59 @@ from .database import get_menu_item
 from .models import OrderItem
 
 
-def check_price_and_availability(form_data):
+def _check_price_and_availability_v100(item_id):
     """Checks the price and availability of an item."""
-    # Now we expect to see "items" in the body form, but
-    # "item" parameter is still accepted, so we need to process it as well!
-    if "item" in form_data:
-        item_id = form_data.get("item")
-        if not item_id:
-            return None
-        menu_item = get_menu_item(item_id)
-        if not menu_item:
-            return None
-        if not menu_item.available:
-            return None
-        return menu_item.price
-
-    # If "items" are passed in the body form
-    item_ids = form_data.getlist("items")
-    if not item_ids:
+    if not item_id:
         return None
 
-    total_price = Decimal("0.0")
-    for item_id in item_ids:
-        if not item_id:
-            continue
-        menu_item = get_menu_item(item_id)
-        if not menu_item:
+    menu_item = get_menu_item(item_id)
+    if not menu_item:
+        return None
+
+    if not menu_item.available:
+        return None
+
+    return menu_item.price
+
+
+def check_price_and_availability(data):
+    """Checks the price and availability of items added to the cart."""
+    if "item" in data:  # noqa: SIM108
+        items = [data.get("item")]
+    else:
+        items = data.getlist("items")
+
+    total_price = Decimal(0)
+    for item in items:
+        price = _check_price_and_availability_v100(item)
+        if not price:
             return None
-        total_price += menu_item.price
+        total_price += price
 
     return total_price
 
 
-def get_order_items(form_data):
+def _get_order_items_v100(item_id):
     """
-    Converts menu item IDs to OrderItem objects.
+    Converts single menu item ID to OrderItem objects.
 
     The menu item price might change during or after the order is created,
     so we need to record this information properly.
     """
-    item_ids = form_data.getlist("items")
-    # If "items" parameter is missing, check if "item" parameter is present
-    if not item_ids:
-        item_id = form_data.get("item")
-        if not item_id:
-            return None
-        item_ids = [item_id]
+    menu_item = get_menu_item(item_id)
+    if not menu_item:
+        return None
+
+    return [OrderItem(item_id=item_id, name=menu_item.name, price=menu_item.price)]
+
+
+def get_order_items(form_data):
+    """Builds OrderItem list for order creation method."""
+    item_ids = form_data.getlist("items") or [form_data.get("item")]
 
     order_items = []
     for item_id in item_ids:
-        if not item_id:
-            continue
-        menu_item = get_menu_item(item_id)
-        if not menu_item:
-            return None
-        order_items.append(OrderItem(item_id=item_id, name=menu_item.name, price=menu_item.price))
+        new_items = _get_order_items_v100(item_id)
+        order_items.extend(new_items)
 
     return order_items
